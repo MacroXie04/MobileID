66e810c327c93ab1b3e6602e1874476e160240aa
Hongzhe Xie <111697797+MacroXie04@users.noreply.github.com>
2025-11-23 03:11:47 -0800

    Enhance superuser management in initadmin command
    Updated the initadmin command to allow password updates for existing superusers. If a superuser already exists, the command now updates their password and email instead of just notifying the user. This improves user management and security by ensuring superuser credentials can be modified easily.

--- src/core/management/commands/initadmin.py
+++ src/core/management/commands/initadmin.py
@@ -23,9 +23,12 @@ class Command(BaseCommand):
             return
 
         if User.objects.filter(username=username).exists():
-            self.stdout.write(
-                self.style.SUCCESS(f'Superuser "{username}" already exists.')
-            )
+            self.stdout.write(f'Superuser "{username}" already exists. Updating password...')
+            user = User.objects.get(username=username)
+            user.set_password(password)
+            user.email = email
+            user.save()
+            self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" password updated.'))
         else:
             self.stdout.write(f'Creating superuser "{username}"...')
             User.objects.create_superuser(
